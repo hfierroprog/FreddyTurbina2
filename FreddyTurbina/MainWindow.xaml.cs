ef01c118c3280374e6007db8e79f7e76b6858dbe
hfierroprog <hfierroprog@gmail.com>
2017-10-16 21:00:25 -0300

    Cambios

---
+++ FreddyTurbina/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace FreddyTurbina
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Posiciona la ventana en centro de la pantalla
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnBicicleta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOfertas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Al hacer click en el personal se ejecutara este codigo:
+        private void btnPersonal_Click(object sender, RoutedEventArgs e)
+        {
+            //Instanciamos a la ventana de personal
+            Personal p = new Personal();
+            //Mostramos la ventana de personal
+            p.Show();
+            //Cerramos la ventana del menú principal
+            Close();
+        }
+    }
+}
---
+++ FreddyTurbina/Personal.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace FreddyTurbina
+{
+    public partial class Personal : Window
+    {
+        public Personal()
+        {
+            InitializeComponent();
+            //Posiciona la ventana en el centro de la pantalla
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+    }
+}
