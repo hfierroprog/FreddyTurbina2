532c28d4c2a823bda68fcca02d29bd6262e8e065
hfierroprog <hfierroprog@gmail.com>
2017-10-18 01:08:08 -0300

    Cambios..

---
+++ BibliotecaOfertas/oferta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotecaOfertas
+{
+    public class oferta
+    {
+        public int Aro{ get; set; }
+        public String Marca{ get; set; }
+        public String Descripcion{ get; set; }
+
+        public oferta(String Marca,int Aro,String Descripcion)
+        {
+            this.Aro = Aro;
+            this.Marca = Marca;
+            this.Descripcion = Descripcion;
+        } 
+    }
+}
---
+++ FreddyTurbina/Ofertas.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FreddyTurbina
+{
+    /// <summary>
+    /// Lógica de interacción para Ofertas.xaml
+    /// </summary>
+    public partial class Ofertas : Window
+    {
+        public Ofertas()
+        {
+            InitializeComponent();
+            //Posiciona la ventana en el centro de la pantalla
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+    }
+}
